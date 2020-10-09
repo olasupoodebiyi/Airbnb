9ed9fa0ad507999f5829b3c69e9a85ea3b9a4eb8
Olasupo <olasupoodebiyi@yahoo.com>
2020-10-09 10:39:42 +0100

    Templating Begins

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
 from rooms import views as room_views
 
-urlpatterns = [path("", room_views.all_rooms, name="home"), path("login")]
+app_name = "core"
+
+urlpatterns = [path("", room_views.all_rooms, name="home")]
