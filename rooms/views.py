14db2da1f9b5025c2d97bbeb1323c59eb2f090d5
Olasupo <olasupoodebiyi@yahoo.com>
2020-10-10 09:42:44 +0100

    Deployed first CBV

--- rooms/views.py
+++ rooms/views.py
@@ -1,20 +1,23 @@
-from math import ceil
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.utils import timezone
+from django.views.generic import ListView
 from . import models
 
 # Create your views here.
 
 
-def all_rooms(request):
-    page = request.GET.get("page")
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10)
-    rooms = paginator.get_page(page)
-    # print(vars(rooms.paginator))
+class HomeView(ListView):
 
-    return render(
-        request,
-        "rooms/home.html",
-        {"rooms": rooms},
-    )
+    """ HomeView Definition """
+
+    model = models.Room
+    paginate_by = 10
+    ordering = "created"
+    paginate_orphans = 5
+    page_kwarg = "page"
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context["now"] = now
+        return context
